83dce2b1b2c891ced5235973a947b0b501bfa2b8
khauee <khauebraga55@gmail.com>
2026-05-08 11:13:25 -0300

    Fazendo exercicio de exemplo

---
+++ fundamentos/src/secao12_enumeracao_composicao/composicao/exemplo01/entities/Department.java
@@ -0,0 +1,17 @@
+package secao12_enumeracao_composicao.composicao.exemplo01.entities;
+
+public class Department {
+    private String name;
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ fundamentos/src/secao12_enumeracao_composicao/enumeracao/exemplo01/entitites/OrderStatus.java
@@ -0,0 +1,8 @@
+package secao12_enumeracao_composicao.enumeracao.exemplo01.entitites;
+
+public enum OrderStatus {
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+}
